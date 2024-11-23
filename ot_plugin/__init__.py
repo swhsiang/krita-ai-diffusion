866fb2763796d9cab8dfff0615656ad4f5f83e2f
Wen Hsiang Shih <wshih@abnormalsecurity.com>
2024-11-23 03:34:08 -0500

    update submodule

--- ot_plugin/__init__.py
+++ ot_plugin/__init__.py
@@ -12,6 +12,13 @@ if not importlib.util.find_spec(".websockets.src", "ot_plugin"):
         " https://github.com/Acly/krita-ai-diffusion/releases"
     )
 
+if not importlib.util.find_spec(".pydantic", "ot_plugin"):
+    raise ImportError(
+        "Could not find pydantic module. This indicates that it was not installed with the"
+        " plugin. Please make sure to download a plugin release package (NOT just the source!). You"
+        " can find the latest release package here:"
+        " https://github.com/Acly/krita-ai-diffusion/releases"
+    )
 
 # The following imports depend on the code running inside Krita, so the cannot be imported in tests.
 if importlib.util.find_spec("krita"):
